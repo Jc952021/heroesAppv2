8dad905bac1e328620d9e5fc93bce96e6370a95c
jc2021 <drakrazer222@gmail.com>
2023-02-25 18:21:22 -0500

    HeroImage Pipe

---
+++ src/app/heroes/pipes/hero-image.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Hero } from '../interfaces/hero.interface';
+
+@Pipe({
+  name: 'heroImage',
+})
+export class HeroImagePipe implements PipeTransform {
+  transform(hero: Hero): string {
+    if (!hero.alt_img && !hero.id) return 'assets/no-image.png';
+
+    if (hero.alt_img) return hero.alt_img;
+
+    return `assets/heroes/${hero.id}.jpg`;
+  }
+}
